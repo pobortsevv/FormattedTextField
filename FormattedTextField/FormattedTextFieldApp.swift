42978bc4b5c68147b89528c149e148364f2d29f8
Владимир Поборцев <Vladimir.Pobortsev@x5.ru>
2022-07-07 19:03:22 +0300

    committing initial files

--- FormattedTextField/FormattedTextFieldApp.swift
+++ FormattedTextField/FormattedTextFieldApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct FormattedTextFieldApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }

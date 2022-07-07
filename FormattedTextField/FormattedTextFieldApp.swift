09d6a9d1a2c182b5600c50404fd6a81f2fa6d3ff
Владимир Поборцев <Vladimir.Pobortsev@x5.ru>
2022-07-07 18:56:45 +0300

    Initial Commit

---
+++ FormattedTextField/FormattedTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextFieldApp.swift
+//  FormattedTextField
+//
+//  Created by sabra on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

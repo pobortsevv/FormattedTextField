21505888ec4dfb3b4751d2a0e0458adc439146cd
Владимир Поборцев <Vladimir.Pobortsev@x5.ru>
2022-07-11 21:41:34 +0300

    Added comments

--- FormattedTextField_iOS/Views/FormattedTextField.swift
+++ FormattedTextField_iOS/Views/FormattedTextField.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+/// Поле ввода форматированного текста.
+///
+/// По сути тот же TextField, только с другой логикой форматирования
+///
+/// Нативное решение:
+///  ```
+///  init(_ title: StringProtocol, value: Binding<V>, Formatter: Formatter)
+///  ```
+///
+/// В данной реализации formatter: (String) -> String, что позволяет нам
+/// переиспользовать уже написанные функции форматирования.
+///
+/// Структура исключает необходимость в Presenter(если он вообще нам понадобиться в SUI)
 public struct FormattedTextField: View {
   
   // MARK: Private properties
@@ -17,6 +30,10 @@ public struct FormattedTextField: View {
   
   // MARK: Init
   
+  /// - Parameters:
+  ///   - title: Заголовок текстового представления, описывающий его назначение.
+  ///   - text: Текст для отображения и редактирования.
+  ///   - formatter: Функция форматирования.
   init(_ title: String, text: Binding<String>, formatter: @escaping (String) -> String) {
     self.title = title
     self._text = text
